11c340908d457dbcf06e856a285daf946504582a
blfuentes <epiphonetodie@gmail.com>
2018-03-28 11:36:28 +0200

    Added controllers and viewmodels

---
+++ NetCoreAngularWeb/NetCoreAngularWeb/Controllers/AnswerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreAngularWeb.ViewModels;
+using Newtonsoft.Json;
+
+namespace NetCoreAngularWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class AnswerController : Controller
+    {
+        // GET api/answer/all 
+        [HttpGet("All/{questionId}")]
+        public IActionResult All(int questionId)
+        {
+            var sampleAnswers = new List<AnswerViewModel>();
+
+            // add a first sample answer 
+            sampleAnswers.Add(new AnswerViewModel()
+            {
+                Id = 1,
+                QuestionId = questionId,
+                Text = "Friends and family",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            });
+
+            // add a bunch of other sample answers 
+            for (int i = 2; i <= 5; i++)
+            {
+                sampleAnswers.Add(new AnswerViewModel()
+                {
+                    Id = i,
+                    QuestionId = questionId,
+                    Text = String.Format("Sample Answer {0}", i),
+                    CreatedDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                });
+            }
+
+            // output the result in JSON format 
+            return new JsonResult(
+                sampleAnswers,
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+    }
+}
