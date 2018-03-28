53ad7ac82b79ce734dbaee5f04db93ef71542c3d
blfuentes <epiphonetodie@gmail.com>
2018-03-28 11:55:18 +0200

    added RESTful route

--- NetCoreAngularWeb/NetCoreAngularWeb/Controllers/QuizController.cs
+++ NetCoreAngularWeb/NetCoreAngularWeb/Controllers/QuizController.cs
@@ -13,7 +13,43 @@ namespace NetCoreAngularWeb.Controllers
     [Route("api/[controller]")]
     public class QuizController : Controller
     {
-        // GET api/quiz/latest
+        #region RESTful conventions methods 
+        /// <summary> 
+        /// GET: api/quiz/{}id 
+        /// Retrieves the Quiz with the given {id} 
+        /// </summary> 
+        /// <param name="id">The ID of an existing Quiz</param> 
+        /// <returns>the Quiz with the given {id}</returns> 
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // create a sample quiz to match the given request 
+            var v = new QuizViewModel()
+            {
+                Id = id,
+                Title = String.Format("Sample quiz with id {0}", id),
+                Description = "Not a real quiz: it's just a sample!",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            };
+
+            // output the result in JSON format 
+            return new JsonResult(
+                v,
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+        #endregion
+
+        #region Attribute-based routing methods
+        /// <summary> 
+        /// GET: api/quiz/latest 
+        /// Retrieves the {num} latest Quizzes 
+        /// </summary> 
+        /// <param name="num">the number of quizzes to retrieve</param> 
+        /// <returns>the {num} latest Quizzes</returns>
         [HttpGet("Latest/{num}")]
         public IActionResult Latest(int num = 10)
         {
@@ -88,5 +124,6 @@ namespace NetCoreAngularWeb.Controllers
                     Formatting = Formatting.Indented
                 });
         }
+        #endregion
     }
 }
